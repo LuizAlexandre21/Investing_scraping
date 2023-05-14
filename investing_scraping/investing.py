d468923abba813077b46beda3fc367020cc29e7a
LuizAlexandre21 <Luizalexandre21@outlook.com>
2023-05-14 01:46:34 -0300

    Select outside library

---
+++ investing_scraping/investing.py
@@ -0,0 +1,8 @@
+# Pacotes
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains 
+import random
+import time 
+from selenium.webdriver.common.by import By
+import shutil
+from selenium.webdriver.chrome.options import Options
